dc4b26d194951200e27a3a58d5eb0ebf945573de
DavidGarHeredia <dgheredia@protonmail.com>
2021-10-17 20:47:27 +0200

    creating types

--- src/ORInterface.jl
+++ src/ORInterface.jl
@@ -1,5 +1,13 @@
 module ORInterface
 
-# Write your package code here.
+abstract type Problem end
+abstract type Constraint end
+abstract type Solution end
+abstract type Status end
+
+export 	Problem, 
+		Constraint, 
+		Solution, 
+		Status
 
 end
